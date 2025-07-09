6a01e1e91ed8db3ac8fff9d6d42c5012300db19b
Murali V Sri Sai 2200030745 <thedecent420@gmail.com>
2025-07-09 09:43:07 +0530

    modified:   server.js

--- server.js
+++ server.js
@@ -34,3 +34,8 @@ mongoose.connect(process.env.MONGO_URI, {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch(err => console.error(err));
+
+  app.get('/ping', (req, res) => {
+    res.send('pong');
+  });
+  
