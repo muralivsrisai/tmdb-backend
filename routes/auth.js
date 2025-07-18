e0b8f1019beb66154de7df96429ecc5eddaeff74
Murali V Sri Sai 2200030745 <thedecent420@gmail.com>
2025-07-18 11:30:53 +0530

    modified:   routes/auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -13,8 +13,7 @@ const router = express.Router();
 const generateToken = (user) => {
   return jwt.sign(
     { id: user._id, role: user.role },
-    process.env.JWT_SECRET,
-    { expiresIn: '7d' }
+    process.env.JWT_SECRET
   );
 };
 
