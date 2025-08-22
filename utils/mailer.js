555205af75526f33bdc0ebbc42dbea79b99b4978
Murali V Sri Sai 2200030745 <thedecent420@gmail.com>
2025-08-22 07:50:38 +0530

    modified:   utils/mailer.js

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,20 +1,22 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'Gmail', // Or SMTP details
+  service: 'Gmail', // Or configure SMTP
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   }
 });
 
-const sendMail = async ({ to, subject, text }) => {
+const sendMail = async ({ to, subject, text, html }) => {
   const mailOptions = {
     from: `"Gledati Support" <${process.env.EMAIL_USER}>`,
     to,
     subject,
-    text
+    text, // fallback for clients that don't support HTML
+    html  // main HTML content
   };
+
   return transporter.sendMail(mailOptions);
 };
 
