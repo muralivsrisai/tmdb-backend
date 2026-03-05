f997f49d381682cbac0e1047bffec10fe1742350
Murali V Sri Sai 2200030745 <thedecent420@gmail.com>
2026-03-05 16:14:28 +0530

    modified:   utils/mailer.js

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,20 +1,25 @@
-const sgMail = require("@sendgrid/mail");
+const nodemailer = require("nodemailer");
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
 
 const sendMail = async ({ to, subject, text, html }) => {
-  const msg = {
+  const mailOptions = {
+    from: `"Gledati Support" <${process.env.EMAIL_USER}>`,
     to,
-    from: {
-      email: process.env.EMAIL_USER,
-      name: "Gledati Support"
-    },
     subject,
     text,
     html
   };
 
-  return await sgMail.send(msg);
+  return await transporter.sendMail(mailOptions);
 };
 
 module.exports = { sendMail };
