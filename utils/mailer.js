c5618d994450ade628cba6ed9a8e062948d365b4
Murali V Sri Sai 2200030745 <thedecent420@gmail.com>
2026-03-05 15:39:17 +0530

    modified:   utils/mailer.js

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,8 +1,8 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
+  host: "smtp-relay.brevo.com",
+  port: 587,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
